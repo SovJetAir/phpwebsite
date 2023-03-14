3c0652587c10cf1c0aeffa0baa6349483c4d1a04
SovJetAir <Cauwenberghsmartijn@gmail.com>
2023-03-14 11:39:03 +0100

    update

--- database/connectie.php
+++ database/connectie.php
@@ -4,7 +4,7 @@
     $con = mysqli_connect("78.47.104.170", "mysql", "b874da847af3bdaf2435", "martin");
 
     if(mysqli_connect_errno()){
-        echo "geen connectie met database:".mysqli_connect_errno();
+        echo "geen connectie met database: ";
     }
 
 ?>
